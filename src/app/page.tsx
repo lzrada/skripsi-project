65232343f6ef81bfc63f480933e6c7b149a4f57f
Muhammad Lazuardi Al Ghiffary <lazuardialghifary53@gmail.com>
2026-05-07 10:35:37 +0700

    redirect update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const cookieStore = await cookies();
   const role = cookieStore.get("userRole")?.value;
-  const token = cookieStore.get("firebaseToken")?.value;
 
   if (role === "admin") {
     redirect("/admin/dashboard-admin");
